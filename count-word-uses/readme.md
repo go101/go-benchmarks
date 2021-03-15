fcd7b02a96aa4d96a47775be000c45edaf1cb741
Go101 <22589241+go101@users.noreply.github.com>
2021-03-15 11:50:20 -0400

    add a benchmark

---
+++ count-word-uses/readme.md
@@ -0,0 +1,29 @@
+
+### About
+
+This benchmark is to compare different ways to stat word count.
+
+### Result
+
+```
+$ go version
+go version go1.16.2 linux/amd64
+$ go test -bench=.
+goos: linux
+goarch: amd64
+pkg: github.com/go101/go-benchmarks/count-word-uses
+cpu: Intel(R) Core(TM) i5-4210U CPU @ 1.70GHz
+Benchmark_f-4   	     687	   1699257 ns/op
+Benchmark_g-4   	    1898	    618261 ns/op
+Benchmark_h-4   	    1892	    623414 ns/opp
+```
+
+Why the `f` function is slow. The reason is `string(byte_slice)` doesn't make
+duplication in map element getter, but it makes a duplication in map element setter.
+`m[string(w)]++` is equivalent to `m[string(w)] = m[string(w)] + 1`, there is
+map element setter and a map element getter in it. The `g` and `h` functions doesn't
+use map element setter as frequently as the function `f`.
+
+(ref: https://github.com/golang/go/issues/45021)
+
+
---
+++ count-word-uses/word-count_test.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"testing"
+)
+
+var words [][]byte
+
+func init() {
+	var buildWord = func(b byte) []byte {
+		w := []byte{b, b, b}
+		w = append(w, w...)
+		w = append(w, w...)
+		w = append(w, w...)
+		w = append(w, w...)
+		w = append(w, w...)
+		w = append(w, w...)
+		return w
+	}
+	for range[100]struct{}{} {
+		for j := range[100]struct{}{} {
+			w := buildWord(byte(j))
+			words = append(words, w)
+		}
+	}
+	
+	var m map[string]int
+	var check = func(s string) {
+		for j := range[100]struct{}{} {
+			w := buildWord(byte(j))
+			if m[string(w)] != 100 {
+				panic(s + "is wrong: " + string(w))
+			}
+		}
+	}
+	m = f(words)
+	check("f")
+	m = g(words)
+	check("g")
+	m = h(words)
+	check("h")
+}
+
+func f(words [][]byte) map[string]int {
+	var m = make(map[string]int)
+	for _, w := range words {
+		m[string(w)]++
+	}
+	return m
+}
+
+func g(words [][]byte) map[string]int {
+	var m2 = make(map[string]*int)
+	for _, w := range words {
+		if v := m2[string(w)]; v != nil {
+			*v++
+		} else {
+			v = new(int)
+			*v = 1
+			m2[string(w)] = v
+		}
+	}
+	var m = make(map[string]int, len(m2))
+	for k, p := range m2 {
+		m[k] = *p
+	}
+	return m
+}
+
+func h(words [][]byte) map[string]int {
+	var m = make(map[string]int)
+	var stats []int
+	for _, w := range words {
+		if v, ok := m[string(w)]; ok {
+			stats[v]++
+		} else {
+			stats = append(stats, 1)
+			v = len(m)
+			m[string(w)] = v
+		}
+	}
+	for k, c := range m {
+		m[k] = stats[c]
+	}
+	return m
+}
+
+var mf map[string]int
+func Benchmark_f(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mf = f(words)
+	}
+}
+
+var mg map[string]int
+func Benchmark_g(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mg = g(words)
+	}
+}
+
+var mh map[string]int
+func Benchmark_h(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mh = h(words)
+	}
+}
+
+
